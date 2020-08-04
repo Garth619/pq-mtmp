52bc783f348f8f33b4ad29edf5f4dee9052da166
Garrett Cullen <Garth619@users.noreply.github.com>
2020-08-04 16:13:15 -0700

    push

--- wp-content/themes/pq-mtmp/page-templates/includes/homepage_template_parts/section-1.php
+++ wp-content/themes/pq-mtmp/page-templates/includes/homepage_template_parts/section-1.php
@@ -74,7 +74,11 @@
 
   <picture>
 
-    <source media='(min-width: 1170px)' srcset='<?php echo $section_one_image_laptop['url']; ?>'>
+    <source media='(min-width: 1380px)' srcset='<?php bloginfo('template_directory');?>/images/hero-1400.jpg'>
+
+    <source media='(min-width: 1170px)' srcset='<?php bloginfo('template_directory');?>/images/hero-1200.jpg'>
+
+    <source media='(min-width: 768px)' srcset='<?php bloginfo('template_directory');?>/images/hero-tablet.jpg'>
 
     <img id='hero' src="<?php bloginfo('template_directory');?>/images/hero-mobile.jpg" alt="" /><!-- hero -->
     
