d14339a5a6699d4c3dd5b84596f9b8cf4d6e6506
Garrett Cullen <Garth619@users.noreply.github.com>
2020-08-05 12:57:08 -0700

    push

--- wp-content/themes/pq-mtmp/page-templates/includes/homepage_template_parts/section-2.php
+++ wp-content/themes/pq-mtmp/page-templates/includes/homepage_template_parts/section-2.php
@@ -2,35 +2,65 @@
 
 	<div id='sec_two_inner'>
 
-		<?php if ( have_rows( 'section_two_selling_points' ) ) : ?>
-
-			<div id='sec_two_slider'>
-			
-			<?php while ( have_rows( 'section_two_selling_points' ) ) : the_row(); ?>
+		<div id='sec_two_top'>
+		
+			<div id='sec_two_left_slider'>
 			
 				<div class='sec_two_slide'>
-
-					<?php $icon = get_sub_field( 'icon' ); ?>
+				
+					<div class='sec_two_slide_profile_wrapper'>
 					
-					<?php if ( $icon ) { ?>
+						<div class='sec_two_profile'>
 						
-						<img src="<?php echo $icon['url']; ?>" alt="<?php echo $icon['alt']; ?>" />
+							<img src='<?php bloginfo('template_directory');?>/images/client-photo.jpg' alt=''/><!-- name -->
+						
+						</div><!-- sec_two_profile -->
+
+						<div class='sec_two_profile_info'>
+						
+							<img src='<?php bloginfo('template_directory');?>/images/stars.svg' alt=''/><!-- name -->
+
+							<span class='sec_two_profile_two'>Kerry - San Diego, CA</span><!-- sec_two_profile_two -->
+						
+						</div><!-- sec_two_profile_info -->
 					
-					<?php } ?>
+					</div><!-- sec_two_slide_profile_wrapper -->
 
-					<span class="amount"><?php the_sub_field( 'amount' ); ?></span>
+					<div class='sec_two_slide_descrip'>
+						
+							<span>“... they have gone above and beyond to make things as painless as humanly possible.”</span>
+						
+						</div><!-- sec_two_slide_descrip -->
 
-					<span class='double_line'></span><!-- double_line -->
-
-					<span class='type'><?php the_sub_field( 'description' ); ?></span><!-- type -->
-
+						<span class='sec_two_slide_type'>DRUG SIDE eFFECTS</span><!-- sec_two_slide_type -->
+				
 				</div><!-- sec_two_slide -->
+			
+			</div><!-- sec_two_left_slider -->
 
-			<?php endwhile; ?>
+			<div id='sec_two_top_arrows'>
+			
+				<div class='sec_two_arrow_left sec_two_arrow'>
+				
+					
+				
+				</div><!-- sec_two_arrow -->
 
-			</div><!-- sec_two_slider -->
+				<div class='sec_two_arrow_right sec_two_arrow'>
+				
+					
+				
+				</div><!-- sec_two_arrow -->
+			
+			</div><!-- sec_two_top_arrows -->
+		
+		</div><!-- sec_two_top -->
 
-		<?php endif; ?>
+		<div id='sec_two_bottom'>
+		
+			
+		
+		</div><!-- sec_two_bottom -->
 	
 	</div><!-- sec_two_inner -->
 
