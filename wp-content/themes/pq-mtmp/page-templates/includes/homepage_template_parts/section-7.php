d87646fc4c168ee04cc5fad60f7fe49ab5393bad
Garrett Cullen <Garth619@users.noreply.github.com>
2020-08-07 16:37:18 -0700

    push

--- wp-content/themes/pq-mtmp/page-templates/includes/homepage_template_parts/section-7.php
+++ wp-content/themes/pq-mtmp/page-templates/includes/homepage_template_parts/section-7.php
@@ -2,7 +2,45 @@
 
 <div id='sec_seven_inner'>
 
+  <div id='sec_seven_top'>
   
+    <div id='sec_seven_video'>
+    
+      <div id='video_overlay'>
+      
+        <span class='play_button'></span><!-- play_button -->
+      
+      </div><!-- video_overlay -->
+
+      <img src='<?php bloginfo('template_directory');?>/images/video-photo.jpg' /><!-- name -->
+    
+    </div><!-- sec_seven_video -->
+  
+  </div><!-- sec_seven_top -->
+
+  <div id='sec_seven_bottom'>
+
+    <div id='sec_seven_bottom_inner'>
+    
+      <span id='sec_seven_title'>Meet the Attorneys</span><!-- sec_seven_title -->
+  
+      <div id='sec_seven_content'>
+      
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      
+      </div><!-- sec_seven_content -->
+  
+      <a id='sec_seven_learn_more' class='button_five' href=''>Learn More</a><!-- button_five -->
+  
+      </div><!-- sec_seven_bottom_inner -->
+
+    <div id='sec_seven_logo'>
+    
+      <img src='<?php bloginfo('template_directory');?>/images/eb-graphic.svg' /><!-- name -->
+    
+    </div><!-- sec_seven_logo -->
+  
+  </div><!-- sec_seven_bottom -->
 
 </div><!-- sec_seven_inner -->
 
