9630a3f204e298d940e2f973a983e99aa0225094
Garrett Cullen <Garth619@users.noreply.github.com>
2020-08-18 15:45:32 -0700

    push

--- wp-content/themes/pq-mtmp/page-templates/includes/page_banner/template-blog_banner.php
+++ wp-content/themes/pq-mtmp/page-templates/includes/page_banner/template-blog_banner.php
@@ -4,6 +4,10 @@
 	
 		<div id='internal_banner_content'>
 
+      <?php // blog main page conditionals
+      
+      if(is_home() && is_single()) { ?>
+
       <?php if(get_field('internal_banner_blog_title','option')) : ?>
 
         <?php if(get_field('banner_h1_blog','option') == "Yes") : ?>
@@ -30,18 +34,49 @@
         
         <?php endif;?>
 
-        <?php if(get_field('turn_off_button_blog','option') == "Yes") : ?>
+      <?php } ?>
+      
+      <?php // categories conditionals
+      
+        if(is_category()) { ?>
 
-          <?php else: ?>
+        <?php if(get_field('banner_h1_blog','option') == "Yes") : ?>
+
+          <h1 class="internal_title banner_title"><?php single_cat_title(); ?></h1><!-- banner_title -->
+
+          <?php else:?>
+
+          <span class="internal_title banner_title"><?php single_cat_title(); ?></span><!-- banner_title -->
+
+        <?php endif;?>
+
+      <?php } ?>
+
+      <?php if(is_archive() && !is_category()) { ?>
+
+        <?php if(get_field('banner_h1_blog','option') == "Yes") : ?>
+
+          <h1 class="internal_title banner_title"><?php printf( __( '<span>%s</span>'), get_the_date( _x( 'Y', 'yearly archives date format' ) ) ); ?></h1><!-- banner_title -->
+
+          <?php else:?>
+
+          <span class="internal_title banner_title"><?php printf( __( '<span>%s</span>'), get_the_date( _x( 'Y', 'yearly archives date format') ) ); ?></span><!-- banner_title -->
+
+        <?php endif;?>
+
+      <?php } ?>
+
+      <?php if(get_field('turn_off_button_blog','option') == "Yes") : ?>
+
+        <?php else: ?>
 
           <a class="button free_consult_button internal_banner_button" href="#consultation">
 
             <span><?php the_field( 'global_internal_banner_button_verbiage','option'); ?></span>
 
-          </a>
+        </a>
 
-        <?php endif;?>
-
+      <?php endif;?>
 
 		</div><!-- internal_banner_content -->
 
--- wp-content/themes/pq-mtmp/single.php
+++ wp-content/themes/pq-mtmp/single.php
@@ -1,30 +1,27 @@
 <?php get_header(); ?>
 
 <div id="internal_main">
-
-	<div id='two_col_template_with_banner'>
 	
-		<?php get_template_part('page-templates/includes/template','default-page-banner'); ?>	
+	<?php get_template_part('page-templates/includes/page_banner/template','blog_banner'); ?>
+				
+	<div id='page_wrapper'>
 
-	</div><!-- two_col_template_with_banner -->
-	
-	<div class="page_container two_col">
-		
-		<div class="page_content">
+ 			<div id='page_content' class="content">
+				
+					<?php get_template_part( 'loop', 'single' ); ?>
 
-			<?php get_template_part( 'loop', 'single' ); ?>
-			
-		</div><!-- page_content -->
+			</div><!-- page_content -->
 
-		<?php if(!get_field('disable_sidebar')) {
+			<?php if(!get_field('disable_sidebar')) {
 
 			get_sidebar('blog');
 
-		} ?>
-		
-	</div><!-- page_container -->
+			} ?>
+				
+	</div><!-- page_wrapper -->	
 	
-	</div><!-- internal_main -->
+</div><!-- internal_main -->
+
 
 <?php get_footer(); ?>
 
