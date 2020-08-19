e25af506d5de02640dd2b5eb882eb052252d4a94
Garrett Cullen <Garth619@users.noreply.github.com>
2020-08-19 16:08:43 -0700

    push

--- wp-content/themes/pq-mtmp/page-templates/template-testimonials.php
+++ wp-content/themes/pq-mtmp/page-templates/template-testimonials.php
@@ -6,42 +6,119 @@ get_header(); ?>
 
 <div id="internal_main">
 
-	<?php get_template_part('page-templates/includes/template','default-page-banner'); ?>
+	<?php get_template_part('page-templates/includes/page_banner/template','page_banner'); ?>
 	
-	<div class="page_container">
+	<div id='page_wrapper' class='testmonials_page_wrapper'>
+	
+		<div id='testimonials_video_wrapper'>
 
-		<div id='testimonials_wrapper' class="internal_wrapper">
-
-			<?php if ( have_rows( 'testi_page' ) ) : ?>
+			<?php if ( have_rows( 'testimonials_video' ) ) : ?>
 				
-				<?php while ( have_rows( 'testi_page' ) ) : the_row(); ?>
+				<?php while ( have_rows( 'testimonials_video' ) ) : the_row(); ?>
 
-					<div class='single_test'>
-			
-						<?php echo file_get_contents( get_template_directory() . '/images/stars.svg' ); ?>
+					<?php if(get_field('wistia_or_youtube_testi') == "Youtube" ) { ?>
 
-						<span class='single_test_descrip'><?php the_sub_field( 'intro' ); ?></span><!-- single_test_descrip -->
+						<div class="single_video_testimonial video_module">
 
-						<div class='single_test_content content'>
-			
-							<?php the_sub_field( 'description' ); ?>
-			
-						</div><!-- single_test_content -->
+							<a href="https://www.youtube.com/embed/<?php the_sub_field( 'youtube_id_testi' ); ?>" data-lity>
 
-						<span class='double_line'></span><!-- double_line -->
+								<div class='single_video_thumbnail'>
+								
+									<div class='video_overlay'>
 
-						<span class='single_test_name'><?php the_sub_field( 'name' ); ?></span><!-- single_test_name -->
+										<span class='play_button'></span><!-- play_button -->
+
+									</div><!-- video_overlay -->
+
+									<?php if(get_sub_field('video_thumbnail_select_testi') == "Leave Blank" ) { ?>
+
+										<img src="https://img.youtube.com/vi/<?php the_sub_field( 'youtube_id_testi' ); ?>/0.jpg"/>
+
+									<?php } ?>
+
+									<?php if(get_sub_field('video_thumbnail_select_testi') == "Add Thumbnail Image" ) { ?>
+
+										<?php $video_thumbnail_video_center_testi = get_sub_field( 'video_thumbnail_video_center_testi' ); ?>
 		
-					</div><!-- single_test -->
-		
-				<?php endwhile; ?>
+										<img src="<?php echo $video_thumbnail_video_center_testi['url']; ?>" alt="<?php echo $video_thumbnail_video_center_testi['alt']; ?>" />
+									
+									<?php } ?>
 
-			<?php endif; ?>
+								</div><!-- single_video_thumbnail -->
 
-		</div><!-- testimonials_wrapper -->
+								<span class='video_title'><?php the_sub_field( 'video_title_testi' ); ?></span><!-- video_title -->
+
+							</a>
+
+						</div><!-- single_video_testimonial -->
+
+					<?php } ?>
+
+					<?php if(get_field('wistia_or_youtube_testi') == "Wistia" ) { ?>
+
+						<div class="single_video_testimonial video_module">
+
+							<a>
+
+								<div class='single_video_thumbnail'>
+
+									<?php if(get_sub_field('video_thumbnail_select_testi') == "Add Thumbnail Image" ) { ?>
+
+										<div class='mywistia wistia_embed wistia_async_<?php the_sub_field( 'wistia_id_testi' ); ?> popover=true popoverContent=html'></div><!-- mywistia -->
+
+									<?php } ?>
+
+									<?php if(get_sub_field('video_thumbnail_select_testi') == "Leave Blank" ) { ?>
+
+										<div class="mywistia_thumbnail">
+
+											<div class='mywistia wistia_embed wistia_async_<?php the_sub_field( 'wistia_id_testi' ); ?> popover=true popoverContent=thumbnail'></div><!-- mywistia -->
+
+										</div><!-- mywistia_thumbnail -->
+
+									<?php } ?>
+
+									<div class='video_overlay'>
+
+										<span class='play_button'></span><!-- play_button -->
+
+									</div><!-- video_overlay -->
+
+								<?php if(get_sub_field('video_thumbnail_select_testi') == "Add Thumbnail Image" ) { ?>
+
+									<?php $video_thumbnail_video_center_testi = get_sub_field( 'video_thumbnail_video_center_testi' ); ?>
 		
-	</div><!-- page_container -->
+									<img src="<?php echo $video_thumbnail_video_center_testi['url']; ?>" alt="<?php echo $video_thumbnail_video_center_testi['alt']; ?>" />
+	
+								<?php } ?>
+
+								</div><!-- single_video_thumbnail -->
+
+								<span class='video_title'><?php the_sub_field( 'video_title_testi' ); ?></span><!-- video_title -->
+
+							</a>
+							
+
+						</div><!-- single_video_testimonial -->
+
+				<?php } ?>
+
+			<?php endwhile; ?>
+
+		<?php endif; ?>
+
+	</div><!-- testimonials_video_wrapper -->
+
+		
+
+	</div><!-- page_wrapper -->
 	
 </div><!-- internal_main -->
 
 <?php get_footer(); ?>
+
+<?php if(get_field('wistia_or_youtube_testi') == "Wistia" ) { ?>
+
+<script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+
+<?php } ?>
