fd72279322f397f7cd2214ecf041546f87d11546
Garrett Cullen <Garth619@users.noreply.github.com>
2020-08-20 17:36:57 -0700

    push

--- wp-content/themes/pq-mtmp/page-templates/template-bio.php
+++ wp-content/themes/pq-mtmp/page-templates/template-bio.php
@@ -6,45 +6,90 @@ get_header(); ?>
 
 <div id="internal_main">
 
-<div id='att_banner'>
-
-	<div id='att_banner_inner'>
+<?php if(!get_field('disable_banner_new')) { ?>
 	
-		<div id='att_banner_left'>
+	<div id='internal_banner'>
 	
-			<div id='att_banner_header_wrapper'>
+		<div id='internal_banner_content'>
+    
+      <div id='att_banner_left'>
 	
-				<h1 id="internal_header" class='att_header' ><?php the_title();?></h1><!-- att_header -->
+        <div id='att_banner_header_wrapper'>
 
-				<span class='double_line bio_line'></span><!-- double_line -->
+          <h1 class="internal_title banner_title"><?php the_title();?></h1><!-- banner_title -->
 
-				<span id='att_position'><?php the_field( 'position' ); ?></span><!-- att_position -->
-	
-			</div><!-- att_banner_header_wrapper -->
+          <span id='bio_position'>Founding Partner</span><!-- bio_position -->
 
-		</div><!-- att_banner_left -->
+        </div><!-- att_banner_header_wrapper -->
 
-		<div id='att_banner_right'></div><!-- att_banner_right -->
+    </div><!-- att_banner_left -->
 
-	</div><!-- att_banner_inner -->
-
-	<?php get_template_part('page-templates/includes/att_bio_template_parts/template','profile_image'); ?>
-	
-</div><!-- att_banner -->
-
-<div class="page_container two_col">
+    <div id='att_banner_right'></div><!-- att_banner_right -->
 		
-		<div class="page_content att_content content">
-			
-			<?php get_template_part( 'loop', 'page' ); ?>
-			
-		</div><!-- page_content -->
+	
 
-		<?php get_sidebar('bio'); ?>
+  </div><!-- internal_banner_content -->
+
+		<?php $global_internal_banner_image = get_field( 'global_internal_banner_image','option'); ?>
+		<?php $banner_image = get_field( 'banner_image' ); ?>
 		
-	</div><!-- page_container -->
+		<?php if($banner_image) : ?>
+
+			<img id="banner_hero" src="<?php echo $banner_image['url']; ?>" alt="<?php echo $banner_image['alt']; ?>"/><!-- internal_hero -->
+
+			<?php else: ?>
+
+			<img id='banner_hero' src="<?php echo $global_internal_banner_image['url']; ?>" alt="<?php echo $global_internal_banner_image['alt']; ?>"/><!-- internal_hero -->
+
+		<?php endif;?>
+		
+	</div><!-- internal_banner -->
+
+	<?php } ?>
+
+	<div id='page_wrapper'>
 	
-	
+   	<div id='page_content'>
+
+		 <div id="mobile_image" class='att_image_wrapper'>
+
+<?php $attorney_profile = get_field( 'attorney_profile' ); ?>
+
+<?php if ( $attorney_profile ) : ?>
+		
+		<img class="att_image" src="<?php echo $attorney_profile['url']; ?>" alt="<?php echo $attorney_profile['alt']; ?>" />
+
+		<?php else: ?>
+
+		<div class="placeholder">
+
+			<div class='placeholder_inner'></div><!-- placeholder_inner -->
+
+			<img class="att_image" src='<?php bloginfo('template_directory');?>/images/placeholder.jpg' alt="Placeholder Profile Image"/>
+
+		</div><!-- placeholder -->
+
+	<?php endif;?>
+
+	</div><!-- att_image_wrapper -->
+
+ 				<div id='page_content_inner' class="content">
+				
+					<?php get_template_part( 'loop', 'page' ); ?>
+
+				</div><!-- page_content_inner -->
+    	
+    	</div><!-- page_content -->
+
+		
+		 <?php if(!get_field('disable_sidebar')) {
+
+			get_sidebar('bio');
+
+			} ?>
+
+	</div><!-- page_wrapper -->
+
 </div><!-- internal_main -->
 					 
 					 	
--- wp-content/themes/pq-mtmp/sidebar-bio.php
+++ wp-content/themes/pq-mtmp/sidebar-bio.php
@@ -1,61 +1,43 @@
 
-<div id='att_sidebar_wrapper'>
-
-<div id='att_sidebar_inner'>
+<div id="sidebar_wrapper" class="sidebar_bio_wrapper">
 
 <?php get_template_part('page-templates/includes/att_bio_template_parts/template','profile_image'); ?>
-
-	<?php if ( have_rows( 'attorney_accolades' ) ) : ?>
-
-		<div id='att_sidebar_lists'>
-
-		<?php while ( have_rows( 'attorney_accolades' ) ) : the_row(); ?>
 		
-			<div class='att_sidebar_list'>
-
-				<span class='att_list_title'><?php the_sub_field( 'title' ); ?></span><!-- att_list_title -->
-
-				<span class='double_line'></span><!-- double_line -->
-
-				<ul class="att_list_menu">
-
-				<?php if ( have_rows( 'list_items' ) ) : ?>
-
-					<?php while ( have_rows( 'list_items' ) ) : the_row(); ?>
-
-						<li><?php the_sub_field( 'list_item' ); ?>
-				
-							<?php if ( have_rows( 'sub_items' ) ) : ?>
-
-								<ul class="att_list_sub_menu">
+	<div class="sidebar_box sidebar_bio">
 					
-									<?php while ( have_rows( 'sub_items' ) ) : the_row(); ?>
-					
-										<li><?php the_sub_field( 'sub_item' ); ?></li>
-
-									<?php endwhile; ?>
-
-								</ul>
+		<div class='sidebar_bio_list'>
 				
-							<?php endif; ?>
+			<h3>Education</h3>
 
-						</li>
+			<ul>
+				<li>B.S. Criminal Justice, San Diego State University, 1989</li>
+				<li>J.D. California Western School of Law, 1992</li>
+			</ul>
 
-					<?php endwhile; ?>
+		</div><!-- sidebar_bio_list -->
 
-				<?php endif; ?>
+		<div class='sidebar_bio_list'>
+				
+			<h3>Education</h3>
 
-				</ul><!-- att_list_menu -->
+			<ul>
+				<li>B.S. Criminal Justice, San Diego State University, 1989</li>
+				<li>J.D. California Western School of Law, 1992</li>
+			</ul>
 
-			</div><!-- att_sidebar_list -->
+		</div><!-- sidebar_bio_list -->
 
-		<?php endwhile; ?>
+		<div class='sidebar_bio_list'>
+				
+			<h3>Education</h3>
 
-		</div><!-- att_sidebar_lists -->
+			<ul>
+				<li>B.S. Criminal Justice, San Diego State University, 1989</li>
+				<li>J.D. California Western School of Law, 1992</li>
+			</ul>
 
-	<?php endif; ?>
-	
-</div><!-- att_sidebar_inner -->
-
-</div><!-- att_sidebar_wrapper -->
-
+		</div><!-- sidebar_bio_list -->
+			
+	</div><!-- sidebar_box -->
+			
+</div><!-- sidebar_wrapper -->
