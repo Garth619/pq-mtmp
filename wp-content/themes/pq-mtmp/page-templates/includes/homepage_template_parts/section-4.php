2bb6b1cd51f5ce21e15d9f3d8fca419f443e1499
Garrett Cullen <Garth619@users.noreply.github.com>
2020-08-22 15:58:20 -0700

    push

--- wp-content/themes/pq-mtmp/page-templates/includes/homepage_template_parts/section-4.php
+++ wp-content/themes/pq-mtmp/page-templates/includes/homepage_template_parts/section-4.php
@@ -2,15 +2,45 @@
 
 	<div id='sec_four_inner'>
 
-		<span id='sec_four_title'>Latest Case Results</span><!-- sec_four_title -->
+		<span id='sec_four_title'><?php the_field( 'section_four_title' ); ?></span><!-- sec_four_title -->
 
 		<div id='sec_four_slider_wrapper'>
 		
 			<div class='sec_four_arrow_left sec_four_arrow'></div><!-- sec_four_arrow -->
 
 			<div id='sec_four_slider'>
-			
-				<div class='sec_four_slide one'>
+
+			<?php if ( have_rows( 'section_four_slider' ) ) : ?>
+				<?php while ( have_rows( 'section_four_slider' ) ) : the_row(); ?>
+
+				<div class='sec_four_slide <?php the_sub_field( 'class' ); ?>'>
+
+					<a href="<?php the_field( 'section_four_case_results_page_link' ); ?>">
+
+						<?php $svg = get_sub_field( 'svg' ); ?>
+		
+						<?php if ( $svg ) { ?>
+							
+							<img src="<?php echo $svg['url']; ?>" alt="<?php echo $svg['alt']; ?>" />
+						
+						<?php } ?>
+
+						<span class='sec_four_slide_title'><?php the_sub_field( 'amount' ); ?></span><!-- sec_four_slide_title -->
+
+						<span class='sec_four_slide_type'><?php the_sub_field( 'type' ); ?></span><!-- sec_four_slide_type -->
+
+						<span class='sec_four_slide_descrip'><?php the_sub_field( 'description' ); ?></span><!-- sec_four_slide_descrip -->
+
+					</a>
+
+				</div><!-- sec_four_slide -->
+
+				<?php endwhile; ?>
+			<?php endif; ?>
+
+			<div class='sec_four_slide one'>
+
+				<a href="">
 				
 					<img src='<?php bloginfo('template_directory');?>/images/result-icon-1.svg' alt=''/><!-- name -->
 
@@ -19,6 +49,8 @@
 					<span class='sec_four_slide_type'>Drug Side Effects</span><!-- sec_four_slide_type -->
 
 					<span class='sec_four_slide_descrip'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span><!-- sec_four_slide_descrip -->
+
+				</a>
 				
 				</div><!-- sec_four_slide -->
 
